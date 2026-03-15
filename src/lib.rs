69ffd87a19575bae6d11affec60a047d6b422b8b
mickleon <mickleon64@gmail.com>
2026-03-15 14:18:03 +0400

    docs: include to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use std::collections::{HashMap, HashSet};
 
+#[doc = include_str!("docs/DFAutomaton/DFAutomaton.md")]
 pub struct DFAutomaton<const ALPHABET_LEN: usize> {
     init_state: u32,
     accept_states: HashSet<u32>,
@@ -10,6 +11,7 @@ pub struct DFAutomaton<const ALPHABET_LEN: usize> {
 impl<const ALPHABET_LEN: usize> DFAutomaton<ALPHABET_LEN> {
     const ALPHABET_LEN: usize = ALPHABET_LEN;
 
+    #[doc = include_str!("docs/DFAutomaton/from.md")]
     pub fn from(
         init_state: u32,
         accept_states: HashSet<u32>,
@@ -33,14 +35,23 @@ impl<const ALPHABET_LEN: usize> DFAutomaton<ALPHABET_LEN> {
         }
     }
 
+    /// Puts the DFA to init state.
     pub fn put_to_init_state(&mut self) {
         self.current_state = self.init_state;
     }
 
+    // Returns a current state.
     pub fn get_current_state(&self) -> u32 {
         self.current_state
     }
 
+    /// Gets an input symbol and puts the DFA in the appropriate state according to the transitions map, depending on the current state.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the transition for symbol not defined in transitions map.
+    ///
+    /// Or if elements of `input` are greater or equal of the `ALPHABET_LEN`.
     pub fn transition(&mut self, symbol: usize) {
         if symbol >= Self::ALPHABET_LEN {
             panic!("Undefined input symbol: \"{}\"", symbol);
@@ -55,6 +66,13 @@ impl<const ALPHABET_LEN: usize> DFAutomaton<ALPHABET_LEN> {
         self.current_state = new_state;
     }
 
+    /// Gets a iterator `input` of DFA symbols and returns `true` if the DFA accepts this string, `false` otherwise.
+    ///
+    /// Resets the current state to the initial state before computing.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the row elements cause the `transition()` function to panic.
     pub fn is_accepting<I>(&mut self, input: I) -> bool
     where
         I: IntoIterator<Item = usize>,
